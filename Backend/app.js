c67263de6e8833c2055e8b04b0ba39bb39603047
Sanath S Karanth <sanathsk97@gmail.com>
2021-05-15 13:43:18 +0530

    code update

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,11 @@
-console.log("My project");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<form method="POST"><input type="text" name="username"><button type="submit">Create user</button></form>')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
